241d491c556eecc9bb5f568867ff73dd8401b92e
porcariasuteis <43611319+porcariasuteis@users.noreply.github.com>
2018-09-27 08:51:28 -0300

    Delete fattree.sh

--- fattree.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd scripts/
-nohup sudo sh ./controller_ecmp.sh &
-cd ..
-sudo python lib/mn_ft.py -i lib/teste -d results/  -p 0.03 -t 10 --ecmp --iperf
-cd results/
-rm -rf h9.out
-sudo sh ./results.sh
-cd ..
